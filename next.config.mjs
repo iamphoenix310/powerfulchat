3a8725b1531ba1f943f8946199ab78419a082ec3
Pankaj Sharma <8818473+iamphoenix310@users.noreply.github.com>
2025-06-03 10:25:00 +0530

    hia

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-import path from 'path'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,7 +23,7 @@ const nextConfig = {
     ]
   },
   experimental: {
-    serverActions: true
+    serverActions: {} // ✅ must be an object
   },
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, '.')
