65c79e522fe9c57283bd6b7ae09c98a31e962c5e
Pankaj Sharma <8818473+iamphoenix310@users.noreply.github.com>
2025-06-03 09:49:05 +0530

    hi

--- app/auth/login/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/auth/login/page.tsx
-import { redirect } from 'next/navigation'
-
-export default function Page() {
-  redirect('https://visitpowerful.com/auth?mode=login')
-}
--- app/auth/sign-up/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/auth/sign-up/page.tsx
-import { redirect } from 'next/navigation'
-
-export default function Page() {
-  redirect('https://visitpowerful.com/auth?mode=signup')
-}
