52d7ae87cb0501fa37e0ab5ce86cddd053df6318
Pankaj Sharma <8818473+iamphoenix310@users.noreply.github.com>
2025-06-13 13:15:47 +0530

    hoja yaar c

--- pages/api/startdownload.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-    // Wait for 30 seconds
-    await new Promise(resolve => setTimeout(resolve, 50000));
-  
-    // Send a response indicating the countdown is complete
-    res.status(200).json({ message: 'Countdown Complete' });
-  }
-  
